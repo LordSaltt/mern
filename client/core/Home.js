56f0e7800bc4fded62a6a7063b0e421529cc75e3
Josue Salinas <jsalinas.us@gmail.com>
2021-08-10 18:56:40 -0400

    Initializing repo

---
+++ client/core/Home.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from 'material-ui/styles'
+import Card, {CardContent, CardMedia} from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import background from './../assets/back1.jpg'
+import {Link} from 'react-router-dom'
+
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px 
+    ${theme.spacing.unit * 2}px`,
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+}) 
+
+
+class Home extends Component {
+  render() {
+    const {classes} = this.props 
+    return (
+      <div>
+        <Card className={classes.card}>
+          <Typography type="headline" component="h2" className=
+          {classes.title}>
+            Home Page
+          </Typography>
+          <CardMedia className={classes.media} image={background} 
+          title="Unicorn Shells"/>
+          <CardContent>
+            <Typography type="body1" component="p">
+              Welcome to the Mern Skeleton home page
+              <Link to="/users">Users</Link>
+              <Link to="/signup">Signup</Link>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+
+export default withStyles(styles)(Home)
